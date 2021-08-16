12923e7c2cc81f89b040469e6074f8c13bbe3372
luisalvaro.leon <luisalvaro.leon@globant.com>
2021-08-15 21:45:20 -0500

    init commit

---
+++ src/main/kotlin/com/example/kotlin/Product/ProductApplication.kt
@@ -0,0 +1,22 @@
+package com.example.kotlin.Product
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@SpringBootApplication
+class ProductApplication
+
+fun main(args: Array<String>) {
+	runApplication<ProductApplication>(*args)
+}
+
+@RestController
+@RequestMapping("/api")
+class HelloController{
+	@GetMapping
+	fun hello() = "Hello World !!"
+
+}
